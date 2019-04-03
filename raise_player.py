84a74313c0451229f3577fb631070e3aa25f7bd9
Aadit <aadit.k12@gmail.com>
2019-04-03 19:30:44 +0800

    Add 3D tensor representation for a hand

--- raise_player.py
+++ raise_player.py
@@ -5,32 +5,38 @@ from pypokerengine.players import BasePokerPlayer
 import numpy as np
 
 class RaisedPlayer(BasePokerPlayer):
-    card_matrix = np.empty((1, 1))
+    # array of cards ranks obtained from PyPokerEngine
+    ranks = [Card.RANK_MAP[key] for key in Card.RANK_MAP]
+    # array of card suits obtained from PyPokerEngine
+    suits = [Card.SUIT_MAP[key] for key in Card.SUIT_MAP]
+    # size of card_tensor
+    card_tensor_size = len(ranks) + len(suits)
+    # han
+    hand_size = 5
 
     """
     Forms a 17x17 matrix of cards by forming combinations of ranks and suits and padding remaining values with zeroes.
     """
     @classmethod
-    def init_card_matrix(self):
-        # array of cards ranks obtained from PyPokerEngine
-        ranks = [Card.RANK_MAP[key] for key in Card.RANK_MAP]
-        # array of card suits obtained from PyPokerEngine
-        suits = [Card.SUIT_MAP[key] for key in Card.SUIT_MAP]
-        matrix_size = len(ranks) + len(suits)
-        RaisedPlayer.card_matrix = np.chararray((matrix_size, matrix_size), itemsize=2)
-        # padding matrix with 0s
-        RaisedPlayer.card_matrix.fill('0')
-        for i in range(len(suits)):
-            cards = [suits[i] + rank for rank in ranks]
-            rank_indices = list(range(len(ranks)))
-            np.put(RaisedPlayer.card_matrix[i], rank_indices, cards)
+    def get_card_tensor(self, card):
+        card_tensor = np.zeros((RaisedPlayer.card_tensor_size, RaisedPlayer.card_tensor_size))
+        row_index = RaisedPlayer.suits.index(card[0])
+        col_index = RaisedPlayer.ranks.index(card[1])
+        card_tensor[row_index][col_index] = 1
+        return card_tensor
 
     # TODO: return correct action and amount (currently randomly chosen)
     def declare_action(self, valid_actions, hole_card, round_state):
-        RaisedPlayer.init_card_matrix()
-        action_dict = choice(valid_actions)
+        hand_tensor = np.zeros((RaisedPlayer.hand_size, RaisedPlayer.card_tensor_size, RaisedPlayer.card_tensor_size))
+        ctr = 0
+        for card in hole_card:
+            hand_tensor[ctr] = RaisedPlayer.get_card_tensor(card)
+            ctr += 1
 
+        # placeholder for the actual action to be taken by the agent
+        action_dict = choice(valid_actions)
         action = action_dict['action']
+
         return action
 
     def receive_game_start_message(self, game_info):
